dad56317df4d8bade059deb49e7d0369b654e3f2
Parth Dasawant <80618762+parthdasawant@users.noreply.github.com>
2023-06-06 23:50:25 +0530

    creation of custom widget using statelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,38 @@
 
+
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Flutter App'),
-    ),
-    body: Center(
-      child: Text('Body is working'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {   },
-      child: Text('+'),
-    ),
-  ),
+  home: Home()
 ));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        backgroundColor: Colors.teal[400],
+      ),
+
+      body: Center(
+        child: Text('Body is working',
+          style: TextStyle(
+            fontFamily: 'Ubuntu',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { },
+        child: Text('CLICK'),
+        backgroundColor: Colors.teal[400],
+      ),
+    );
+  }
+}
+
