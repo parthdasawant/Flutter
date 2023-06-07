b50805d3f3d0721541c24d978a8bc70aad478fef
Parth Dasawant <80618762+parthdasawant@users.noreply.github.com>
2023-06-07 15:34:22 +0530

    id card complete

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,73 @@ class Idcard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[700] ,
       ),
-      body: Center(
-        child: Container(
+      body: Padding(
 
-          child: const Text('Body is working',
-          style: TextStyle(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const SizedBox(height: 25,),
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/avatar.jpg'),
+                radius: 80.0,), ),
+               // const SizedBox(height: 20.0,),
+               Divider(height: 90.0,
+               color: Colors.grey[200],),
+               const Text('NAME',
+                  style: TextStyle(
+                    letterSpacing: 1.0,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+              const SizedBox(height: 5),
+              const Text('NEOTON',
+                style: TextStyle(
+                  letterSpacing: 2.0,
+                  fontSize: 45.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
 
-          ),),
-        ),
-      ),
-    );
+                ),
+              ),
+              const SizedBox(height: 40),
+              const Text('LEVEL',
+                style: TextStyle(
+                  letterSpacing: 1.0,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('NAIVE',
+                style: TextStyle(
+                  letterSpacing: 2.0,
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                ),
+              ),
+              const SizedBox(height: 40),
+              const Row(
+                children: [
+                  Icon(Icons.email, color: Colors.grey,),
+                  SizedBox(width: 10,),
+                  Text('neoton@gamil.com',
+                  style: TextStyle(
+                    letterSpacing: 2.0,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber,
+                  ),)
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
